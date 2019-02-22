5421d1ce73d0c8f2c206dbe7b1e657310e801492
ajesquirell <aesquirell@liberty.edu>
2019-02-22 14:45:04 -0500

    Spatial mapping stuff
    Empty placeholder gameobject to enable tap to place objects. Working with a treasure chest asset right now when you tap anywhere in the scene. Basically following the following website, with a few changes because of outdatedness. https://mtaulty.com/category/holotoolkit-unity/page/5/

---
+++ ENGR482_Team11/Assets/Austin Scripts/Placeholder.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity;
+
+namespace HoloToolkit.Unity.InputModule
+{
+    public class Placeholder : MonoBehaviour, IInputClickHandler
+    {
+
+        public Transform prefab;
+
+        // Use this for initialization
+        void Start()
+        {
+            InputManager.Instance.PushFallbackInputHandler(this.gameObject);
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void OnInputClicked(InputClickedEventData eventData)
+        {
+            //throw new System.NotImplementedException();
+            Transform instance = Instantiate(this.prefab);
+
+            instance.gameObject.transform.position =
+                GazeManager.Instance.GazeOrigin +
+                GazeManager.Instance.GazeNormal * 1.5f;
+           // var tapToPlace = instance.gameObject.AddComponent<TapToPlace>();
+        }
+    }
+}
+
+
