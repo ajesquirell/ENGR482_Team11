c9821ea7b5041994197e7c242328c07e4d22d521
ajesquirell <aesquirell@liberty.edu>
2019-02-23 14:55:06 -0500

    Added some Tap to place Scripts, Placeholder still places Chest asset

--- ENGR482_Team11/Assets/Austin Scripts/Placeholder.cs
+++ ENGR482_Team11/Assets/Austin Scripts/Placeholder.cs
@@ -1,38 +1,52 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using HoloToolkit.Unity.InputModule;
 using HoloToolkit.Unity;
 
-namespace HoloToolkit.Unity.InputModule
-{
+
+//namespace HoloToolkit.Unity.InputModule
+//{
     public class Placeholder : MonoBehaviour, IInputClickHandler
     {
 
         public Transform prefab;
 
         // Use this for initialization
-        void Start()
+        private void Start()
         {
             InputManager.Instance.PushFallbackInputHandler(this.gameObject);
         }
 
         // Update is called once per frame
-        void Update()
+        private void Update()
         {
+            if (!this.loaded && (WorldAnchorManager.Instance.AnchorStore != null))
+            {
+                var ids = WorldAnchorManager.Instance.AnchorStore.GetAllIds();
 
+                //NB: I'm assuming that the order here is either preserved or maybe doesn't matter.
+                foreach (var id in ids)
+                {
+                 var instance = Instantiate(this.prefab);
+                 WorldAnchorManager.Instance.AttachAnchor(instance.gameObject, id);
+                }
+                this.loaded = true;
+                this.count = ids.Length;
+            }
         }
 
         public void OnInputClicked(InputClickedEventData eventData)
         {
-            //throw new System.NotImplementedException();
-            Transform instance = Instantiate(this.prefab);
+            Transform instance = Instantiate(prefab);
 
             instance.gameObject.transform.position =
                 GazeManager.Instance.GazeOrigin +
                 GazeManager.Instance.GazeNormal * 1.5f;
-           // var tapToPlace = instance.gameObject.AddComponent<TapToPlace>();
         }
+    bool loaded;
+    int count;
     }
-}
+//}
 
 
