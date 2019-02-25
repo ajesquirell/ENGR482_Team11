4f5b84632f9ffec5585ceb3710d263e0a473d1d1
ajesquirell <aesquirell@liberty.edu>
2019-02-25 00:58:48 -0500

    Adding Object collections
    Working with trying to instantiate object collections when an input hologram is pressed

---
+++ ENGR482_Team11/Assets/Austin Scripts/HoldAnywhereHandler.cs
@@ -0,0 +1,44 @@
+﻿using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity.SpatialMapping;
+using UnityEngine;
+
+public class HoldAnywhereHandler : MonoBehaviour, IHoldHandler
+{
+    /// <summary>
+    /// Game object to send a fallback hold event to, using a custom "AnywhereHoldCompleted" event.
+    /// </summary>
+    public GameObject SendEventToObject;
+
+    // Use this for initialization
+    void Start()
+    {
+        Debug.Log(gameObject.name + " : Registering as fallback input handler.");
+        // Register using the singleton input manager instance of the HoloToolkit 
+        InputManager.Instance.PushFallbackInputHandler(gameObject);
+    }
+
+    public void OnHoldCompleted(HoldEventData eventData)
+    {
+        Debug.Log(gameObject.name + " : Received fallback OnHoldCompleted event. " + Time.time);
+        if (SendEventToObject != null && SendEventToObject.GetComponent<TapToPlace>() != null)
+        {
+            // Send custom event to the registered game object
+            var receivingObject = SendEventToObject.GetComponent<TapToPlace>();
+            if (!receivingObject.IsBeingPlaced)
+            {
+                receivingObject.OnInputClicked(null);
+            }
+            //SendEventToObject.SendMessage("AnywhereHoldCompleted", null);
+        }
+    }
+
+    public void OnHoldStarted(HoldEventData eventData)
+    {
+        // Implementation not needed, but method required from interface
+    }
+
+    public void OnHoldCanceled(HoldEventData eventData)
+    {
+        // Implementation not needed, but method required from interface
+    }
+}
