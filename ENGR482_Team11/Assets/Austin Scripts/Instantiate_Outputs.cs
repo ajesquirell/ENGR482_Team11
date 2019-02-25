3c547c6843b4d798327134a1545b65643b8cdd9b
ajesquirell <aesquirell@liberty.edu>
2019-02-25 03:50:59 -0500

    More updates
    Output holograms by clicking on input holograms works. - new script called Instantiate_Outputs.
    Currently for "station 1" there is a prefab called "station 1 output holograms" that contains the "plate" from the bounding box rig example with a picture and two other random objects with pictures on them. 2 hand manipulation works with the plate, and "normal" bounding box stuff works with the other 2 objects.
    
    BUG: When destroying the output holograms, the bounding box and app bar components/objects stick around, and they screw up the ability to click on things when you spawn the objects again with the root hologram. Just need to somehow also destroy these along with the holograms themselves.

---
+++ ENGR482_Team11/Assets/Austin Scripts/Instantiate_Outputs.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+
+public class Instantiate_Outputs : MonoBehaviour, IInputClickHandler {
+
+    public GameObject outputCollection;
+    private GameObject instantiatedObj;
+    bool active = false;
+
+    public void OnInputClicked(InputClickedEventData eventData)
+    {
+        Debug.Log("Input: Pressed");
+
+        if (!active)
+        {
+            instantiatedObj = Instantiate(outputCollection, outputCollection.transform.position, outputCollection.transform.rotation);
+            active = true;
+        }
+        else
+        {
+            Destroy(instantiatedObj);
+            //Destroy(instantiatedObj.GetComponentsInChildren();
+            active = false;
+        }
+    }
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
