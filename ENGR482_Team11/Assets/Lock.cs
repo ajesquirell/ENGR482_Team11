d76f2307e4189d0d9a89297c214ec2799e276a25
ajesquirell <aesquirell@liberty.edu>
2019-03-27 11:21:12 -0400

    For DG7
    Lock Root holograms from moving (eventually attach worldanchors here)
    Add tutorial and say "ok" to make it go away

---
+++ ENGR482_Team11/Assets/Lock.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using HoloToolkit.Unity.InputModule;
+using HoloToolkit.Unity;
+using HoloToolkit.Unity.UX;
+using HoloToolkit.Unity.InputModule.Utilities.Interactions;
+
+public class Lock : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void lockRoot()
+    {
+        //Simply disable the components that we use to move
+        if (gameObject.GetComponent<HandDraggable>())
+            gameObject.GetComponent<HandDraggable>().enabled = false;
+        if (gameObject.GetComponent<BoundingBoxRig>())
+            gameObject.GetComponent<BoundingBoxRig>().enabled = false;
+        if (gameObject.GetComponent<TwoHandManipulatable>())
+            gameObject.GetComponent<TwoHandManipulatable>().enabled = false;
+
+    }
+
+    public void unlockRoot()
+    {
+        
+        //if (gameObject.GetComponent<HandDraggable>())
+        //    gameObject.GetComponent<HandDraggable>().enabled = true;
+        //if (gameObject.GetComponent<BoundingBoxRig>())
+        //    gameObject.GetComponent<BoundingBoxRig>().enabled = true;
+        if (gameObject.GetComponent<TwoHandManipulatable>())
+            gameObject.GetComponent<TwoHandManipulatable>().enabled = true;
+
+    }
+}
