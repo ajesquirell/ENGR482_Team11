6841428df61ab4b80b6e99dcfd02ad26e39ccd75
ajesquirell <aesquirell@liberty.edu>
2019-05-17 12:04:19 -0400

    Full Edit mode

---
+++ ENGR482_Team11/Assets/Austin Scripts/EditModeScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EditModeScript : MonoBehaviour {
+
+    public GameObject EditModeMenu;
+    public GameObject EditSpeechManager;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void EnterEditMode()
+    {
+        EditModeMenu.SetActive(true);
+        EditSpeechManager.SetActive(true);
+    }
+
+    public void ExitEditMode()
+    {
+        EditModeMenu.SetActive(false);
+        EditSpeechManager.SetActive(false);
+    }
+}
